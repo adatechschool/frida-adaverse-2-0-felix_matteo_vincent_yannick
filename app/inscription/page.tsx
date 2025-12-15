80eeeae50c8514abab1cd871cabcd7261ad9e5da
VincentComier <V.Comier@hotmail.fr>
2025-12-15 15:23:07 +0100

    add: signin page & add: redirect post to id

---
+++ app/inscription/page.tsx
@@ -0,0 +1,10 @@
+import {SignUpForm} from "@/app/components/connectionForms/SignUpForm";
+
+export default async function Inscription() {
+    return (
+        <main>
+            <h1>Inscription</h1>
+            <SignUpForm />
+        </main>
+    );
+}

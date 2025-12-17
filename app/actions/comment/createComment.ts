fae947443258b9a86f1c086be53eab6583954c90
ValotKzm <darkvalot@gmail.com>
2025-12-17 18:56:29 +0100

    fix: working comment display

--- app/actions/comment/createComment.ts
+++ app/actions/comment/createComment.ts
@@ -2,28 +2,25 @@
 
 import { auth } from "@/auth";
 import { db } from "@/lib/db/drizzle";
-import { comment, post, user } from "@/lib/db/schema";
-import { eq } from "drizzle-orm";
+import { comment } from "@/lib/db/schema";
 import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 
 export const createComment = async (formData: FormData) => {
+  const session = await auth.api.getSession({ headers: await headers() });
 
-    const session = await auth.api.getSession({ headers: await headers() });
+  if (!session?.user?.id) {
+    throw new Error("Utilisateur non authentifié");
+  }
 
-    if (!session?.user?.id) {
-        throw new Error("Utilisateur non authentifié");
-    }
+  const content = formData.get("content") as string;
+  const currentPostId = formData.get("postId") as string;
 
-    const content = formData.get("content") as string;
-    const currentPostId = formData.get("postId") as string;
-    const userId = formData
+  const newComment = await db.insert(comment).values({
+    userId: session.user.id,
+    postId: Number(currentPostId),
+    content: content as string,
+  });
 
-    const newComment = await db.insert(comment).values({
-        userId: session.user.id,
-        postId: Number(currentPostId),
-        content: content as string,
-    });
-
-    revalidatePath("/");
+  revalidatePath("/");
 };
--- app/components/comment/CreateComment.tsx
+++ app/components/comment/CreateComment.tsx
@@ -2,14 +2,19 @@
 
 import { createComment } from "@/app/actions/comment/createComment";
 
-export const CreateComment = ({id}:{id:number}) => {
-
-    return (
-        <form action={createComment} className="flex flex-col">
-            <label htmlFor="content">Commentaire</label>
-            <textarea name="content" placeholder="respectez les règles de la communauté 😜" required />
-            <input type="hidden" name="postId" value={String(id)}/>
-            <button type="submit" className=" bg-blue-200">Publier</button>
-        </form>
-    );
+export const CreateComment = ({ id }: { id: number }) => {
+  return (
+    <form action={createComment} className="flex flex-col">
+      <label htmlFor="content">Commentaire</label>
+      <textarea
+        name="content"
+        placeholder="respectez les règles de la communauté 😜"
+        required
+      />
+      <input type="hidden" name="postId" value={String(id)} />
+      <button type="submit" className=" bg-blue-200">
+        Publier
+      </button>
+    </form>
+  );
 };

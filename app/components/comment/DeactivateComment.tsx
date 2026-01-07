58aa4c038056719b313844a3604c36104802b692
Félix Prudhomme <felixprudhomme@yahoo.com>
2026-01-07 11:12:08 +0100

    update: responsive post

--- app/components/comment/DeactivateComment.tsx
+++ app/components/comment/DeactivateComment.tsx
@@ -7,7 +7,7 @@ export const DeactivateComment = ({commentId}: {commentId: number}) => {
         await deactivateComment(id);
     return (
         <button onClick={() => handleDelete(commentId)} className="mt-2 px-4 py-2 bg-red-200">
-            Supprimer le commentaire
+            Supprimer
         </button>
     );
 };
